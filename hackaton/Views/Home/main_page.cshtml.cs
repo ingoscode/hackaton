6b354863b78e3a73834286d930496d3b984d1893
kishik <kishik.maxim@gmail.com>
2019-08-09 04:08:06 +0300

    night

---
+++ hackaton/Views/Home/main_page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hackaton.Views
+{
+    public class main_page : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ hackaton/Views/Home/second_page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hackaton.Views.Home
+{
+    public class second_page : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
